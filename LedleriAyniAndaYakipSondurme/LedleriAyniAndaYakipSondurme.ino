d9fe3aa3b631cc445c54eb51bdbbbc8cd72cd17e
Hüsna Didar Uçar <husnadidarucar@gmail.com>
2026-02-13 15:26:38 +0300

    Turn on and off LEDs at the same time

---
+++ LedleriAyniAndaYakipSondurme/LedleriAyniAndaYakipSondurme.ino
@@ -0,0 +1,20 @@
+void setup() {//Ledleri aynı anda yandırıp söndürme...
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  digitalWrite(3, 1);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, 1);
+  delay(1000);
+  digitalWrite(2,0);
+  digitalWrite(3,LOW);
+  digitalWrite(4,0);
+  digitalWrite(5,0);
+  delay(1000);
+}
